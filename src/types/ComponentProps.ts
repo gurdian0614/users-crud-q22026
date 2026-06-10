b843671ea009517de1893235f63a9f361ece699a
gurdian0614 <ivan.gurdian@outlook.com>
2026-06-09 20:09:00 -0600

    Create types

---
+++ src/types/ComponentProps.ts
@@ -0,0 +1,21 @@
+import type { ChangeEvent, Dispatch, SetStateAction } from "react";
+import type { User } from "./User";
+import type { UserFormData } from "./UserFormData";
+
+type userToEditType = User | null;
+
+export type UserFormProps = {
+    formData: UserFormData;
+    setFormData: Dispatch<SetStateAction<UserFormData>>;
+    handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    userToEdit: userToEditType;
+    setUserToEdit: Dispatch<SetStateAction<userToEditType>>;
+    loading: boolean;
+}
+
+export type UserListProps = {
+    users: User[];
+    handleEdit: (user: User) => void;
+    handleDelete: (id: number) => void;
+    loading: boolean;
+}
